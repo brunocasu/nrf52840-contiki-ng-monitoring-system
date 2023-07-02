a7626390aa22e6ea8838b9dfc01dd67907bd1437
brunocasu <brunocasu8@gmail.com>
2023-07-01 21:36:57 -0400

    mqtt sensor updates

--- src/mqtt_sensor/mqtt-client.c
+++ src/mqtt_sensor/mqtt-client.c
@@ -71,12 +71,12 @@
 static const char *broker_ip = APP_MQTT_BROKER_ADDR;
 static struct etimer pub_data_timer;
 static struct etimer reconnect_timer;
-int n_reconnect_attempts = 0;
-int msg_seq_n = 0;
-int mqtt_connect_status;
+static int n_reconnect_attempts = 0;
+static int msg_seq_n = 0;
+const int sensor_reading = 123;
+// int mqtt_connect_status;
 
-static int build_publish(void);
-static void mqtt_connect_error_handler(const int status);
+// static void mqtt_connect_error_handler(const int status);
 static bool have_connectivity(void);
 static void mqtt_event(struct mqtt_connection *m, mqtt_event_t event, void *data);
 static void pub_handler(const char *topic, uint16_t topic_len, const uint8_t *chunk, uint16_t chunk_len);
@@ -106,8 +106,8 @@ static uint8_t state;
 
 static char client_id[BUFFER_SIZE];
 static char pub_topic[BUFFER_SIZE];
-static char sub_topic[BUFFER_SIZE];
-static int value = 0;
+// static char sub_topic[BUFFER_SIZE];
+// static int value = 0;
 /*---------------------------------------------------------------------------*/
 /*
  * The main MQTT buffers.
@@ -121,21 +121,19 @@ static struct mqtt_connection conn;
 mqtt_status_t status;
 char broker_address[CONFIG_IP_ADDR_STR_LEN];
 
-PROCESS(mqtt_connection_process, "MQTT Connection Handling");
-PROCESS(mqtt_pub_sensor_data_process, "MQTT Publish");
-AUTOSTART_PROCESSES(&mqtt_connection_process);
+PROCESS_NAME(mqtt_client_process);
+AUTOSTART_PROCESSES(&mqtt_client_process);
+
+PROCESS(mqtt_client_process, "MQTT Client");
 
 /*---------------------------------------------------------------------------*/
-PROCESS_THREAD(mqtt_connection_process, ev, data)
+PROCESS_THREAD(mqtt_client_process, ev, data)
 {
 
   PROCESS_BEGIN();
   
-  // Start the child process
-  process_start(&mqtt_pub_sensor_data_process, NULL);
-  
   printf("TEMP MONITORING APP - SENSOR NODE (MQTT Client)\n");
-  printf("PROCESS THREAD: mqtt_connection_process Begin\n");
+  printf("PROCESS THREAD: mqtt_client_process Begin\n");
 
   // Initialize the ClientID as MAC address
   snprintf(client_id, BUFFER_SIZE, "%02x%02x%02x%02x%02x%02x",
@@ -144,18 +142,13 @@ PROCESS_THREAD(mqtt_connection_process, ev, data)
                      linkaddr_node_addr.u8[6], linkaddr_node_addr.u8[7]);
 
   // Broker registration					 
-  mqtt_register(&conn, &mqtt_connection_process, client_id, mqtt_event, MAX_TCP_SEGMENT_SIZE);
+  mqtt_register(&conn, &mqtt_client_process, client_id, mqtt_event, MAX_TCP_SEGMENT_SIZE);
 				  
   state=STATE_INIT;
-				    
-  // Initialize periodic timer to check the status 
-  etimer_set(&reconnect_timer, RECONNECT_PERIOD);
 
   /* Main loop */
   while(1) {
-    PT_YIELD(mqtt_connection_process)  
-    //PROCESS_YIELD();
-    
+      
     if((ev == PROCESS_EVENT_TIMER && data == &reconnect_timer) ||
         ev == PROCESS_EVENT_POLL || 
         state==STATE_INIT){
@@ -170,7 +163,7 @@ PROCESS_THREAD(mqtt_connection_process, ev, data)
                     printf("PROCESS THREAD: Start connection with Broker\n");
                     memcpy(broker_address, broker_ip, strlen(broker_ip));
                     mqtt_connect(&conn, broker_address, DEFAULT_BROKER_PORT,
-                            DEFAULT_PUBLSH_INTERVAL / CLOCK_SECOND,
+                            (DEFAULT_PUBLSH_INTERVAL * 3) / CLOCK_SECOND,
                             MQTT_CLEAN_SESSION_ON);
                     etimer_set(&reconnect_timer, RECONNECT_PERIOD); // used as a timeout
                     // STATE_CONNECING waits for MQTT EVEN to connect
@@ -180,12 +173,13 @@ PROCESS_THREAD(mqtt_connection_process, ev, data)
                     // STATE_INIT is maitained
                     etimer_set(&reconnect_timer, RECONNECT_PERIOD);
                 }
+                break;
+                
             case STATE_CONNECTING:
                 n_reconnect_attempts++;
-                printf("PROCESS THREAD: (%d)Connecting to broker... \n", n_reconnect_attempts);
-                
                 // STATE_CONNECING is maitained
                 if (n_reconnect_attempts < MAX_RECONNECT_ATTEMPTS){
+                    printf("PROCESS THREAD: (%d) Retry broker connection...\n", n_reconnect_attempts);
                     mqtt_connect(&conn, broker_address, DEFAULT_BROKER_PORT,
                             DEFAULT_PUBLSH_INTERVAL / CLOCK_SECOND,
                             MQTT_CLEAN_SESSION_ON);
@@ -195,102 +189,56 @@ PROCESS_THREAD(mqtt_connection_process, ev, data)
                     n_reconnect_attempts = 0;
                     printf("PROCESS THREAD: Failed to connect to broker... \n");
                 }
+                break;
             
             case STATE_CONNECTED:
                 n_reconnect_attempts = 0;
                 etimer_stop(&reconnect_timer);
-                // Subscribe to a topic
-                printf("PROCESS THREAD: Testing subscribe to %s\n", SENSOR_CONFIG_TOPIC);
-                strcpy(sub_topic,SENSOR_CONFIG_TOPIC);
-                mqtt_subscribe(&conn, NULL, sub_topic, MQTT_QOS_LEVEL_0);
-                // if (status == MQTT_SUBSCRIBE_SUCCESS){
-                //     printf("PROCESS THREAD: Subscribe Success %s\n", SENSOR_CONFIG_TOPIC);
-                // }
-                // else{
-                //     printf("PROCESS THREAD: Subscribe Failed %s\n", SENSOR_CONFIG_TOPIC);
-                // }
+                
+                sprintf(pub_topic, "%s", "status");
+			
+                sprintf(app_buffer, 
+                    "{"
+                    "\"d\":{"
+                    "\"SectionID\":\""APP_SECTION_ID"\","
+                    "\"SensorID\":\""APP_SENSOR_ID"\","
+                    "\"DataType\":\""APP_DATA_TYPE"\","
+                    "\"Data\":%d,"
+                    "\"Platform\":\""CONTIKI_TARGET_STRING"\","
+#ifdef CONTIKI_BOARD_STRING
+                    "\"Board\":\""CONTIKI_BOARD_STRING"\","
+#endif
+                    "\"MsgNumber\":%d,"
+                    "\"Uptime (sec)\":%lu", sensor_reading, msg_seq_n, clock_seconds());
+			
+                msg_seq_n++;
+				printf("PROCESS THREAD: Publish message (%d) \n", msg_seq_n);
+                mqtt_publish(&conn, NULL, pub_topic, (uint8_t *)app_buffer, strlen(app_buffer), MQTT_QOS_LEVEL_0, MQTT_RETAIN_OFF);
+                
+                
+                etimer_set(&pub_data_timer, PUBLISH_DATA_PERIOD);
                 // STATE_CONNECTED is maintained until MQTT event detects a disconnection
+                break;
+                
 
             case STATE_DISCONNECTED:
                 // Connect to MQTT server
+                etimer_stop(&pub_data_timer);
                 state = STATE_CONNECTING;
-                printf("PROCESS THREAD: RE-Start connection with Broker\n");
-                memcpy(broker_address, broker_ip, strlen(broker_ip));
-                mqtt_connect(&conn, broker_address, DEFAULT_BROKER_PORT,
-                        DEFAULT_PUBLSH_INTERVAL / CLOCK_SECOND,
-                        MQTT_CLEAN_SESSION_ON);
                 etimer_set(&reconnect_timer, RECONNECT_PERIOD);
                 // STATE_CONNECING waits for MQTT EVEN to connect
+                break;
                 
             default:
                 printf("PROCESS THREAD: Unknown state\n");
                 PROCESS_EXIT();
+                break;
         }
     }
-
+    PROCESS_YIELD();
   }
 
   PROCESS_END();
-}
-
-PROCESS_THREAD(mqtt_pub_sensor_data_process, ev, data)
-{
-    while(1){
-        
-        //PROCESS_YIELD();
-       
-        if((ev == PROCESS_EVENT_TIMER && data == &pub_data_timer) // When the publish timer expires
-            || ev == PROCESS_EVENT_POLL){ // When connection is established the mqtt even polls this thread
-            if (state == STATE_CONNECTED){    
-                // Publish sensor data
-                sprintf(pub_topic, "%s", SENSOR_DATA_TOPIC);
-                
-                if (build_publish() != PUBLISH_DATA_SIZE_ERROR){
-                    printf("PUBLISH SENSOR DATA: Payload\n %s\n", app_buffer);
-                    mqtt_publish(&conn, NULL, pub_topic, (uint8_t *)app_buffer, strlen(app_buffer), MQTT_QOS_LEVEL_0, MQTT_RETAIN_OFF);
-                }
-            }        
-        }
-        etimer_set(&pub_data_timer, PUBLISH_DATA_PERIOD);
-    }
-
-}
-
-static int build_publish(void)
-{
-  int len;
-  int remaining = APP_BUFFER_SIZE;
-  int i;
-  int sensor_reading = 123;
-  char def_rt_str[64];
-
-  msg_seq_n++;
-
-  len = snprintf(app_buffer, remaining,
-                 "{"
-                 "\"d\":{"
-                 "\"SectionID\":\""APP_SECTION_ID"\","
-                 "\"SensorID\":\""APP_SENSOR_ID"\","
-                 "\"DataType\":\""APP_DATA_TYPE"\","
-                 "\"Data\":%d,"
-                 "\"Platform\":\""CONTIKI_TARGET_STRING"\","
-#ifdef CONTIKI_BOARD_STRING
-                 "\"Board\":\""CONTIKI_BOARD_STRING"\","
-#endif
-                 "\"MsgNumber\":%d,"
-                 "\"Uptime (sec)\":%lu",
-                 sensor_reading, msg_seq_n, clock_seconds());
-
-  if(len < 0 || len >= remaining) {
-    LOG_ERR("PUBLISH ERROR: Buffer too short. Have %d, need %d + \\0\n", remaining, len);
-    return PUBLISH_DATA_SIZE_ERROR;
-  }
-  return 1;
-}
-
-static void mqtt_connect_error_handler(const int status){
-    printf("MQTT CONNECT ERROR: Error number: %d", status);
-    return;
 }
 
 /*---------------------------------------------------------------------------*/
@@ -309,14 +257,13 @@ mqtt_event(struct mqtt_connection *m, mqtt_event_t event, void *data)
     case MQTT_EVENT_CONNECTED: {
         printf("MQTT EVENT: Connected to MQTT broker\n");
         state = STATE_CONNECTED;
-        process_poll(&mqtt_connection_process);
-        process_poll(&mqtt_pub_sensor_data_process);
+        process_poll(&mqtt_client_process);
         break;
     }
     case MQTT_EVENT_DISCONNECTED: {
         printf("MQTT EVENT: MQTT broker disconnected. Reason %u\n", *((mqtt_event_t *)data));
         state = STATE_DISCONNECTED;
-        process_poll(&mqtt_connection_process);
+        process_poll(&mqtt_client_process);
         break;
     }
     case MQTT_EVENT_PUBLISH: {
